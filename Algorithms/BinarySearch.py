939e3b93ba08593dcda4b4f57f2cd2c8c886a5d8
joshmaglione <joshmaglione@gmail.com>
2025-01-16 10:01:46 +0000

    comments

--- Algorithms/BinarySearch.py
+++ Algorithms/BinarySearch.py
@@ -1,13 +1,13 @@
 def BinarySearch(L, x):
-	low = 0
-	high = len(L) - 1
-	while low <= high:
-		mid = (low + high)//2
-		guess = L[mid]
+	low = 0							# low is first entry of snippet
+	high = len(L) - 1				# high is last entry of snippet
+	while low <= high:				# TRUE if we can further subdivide
+		mid = (low + high)//2		# find midpoint
+		guess = L[mid]				# a_i to compare with
 		if x == guess:
 			return mid
 		if x < guess:
-			high = mid - 1
+			high = mid - 1			# decrease large endpoint
 		else:
-			low = mid + 1
+			low = mid + 1			# increase small endpoint
 	return None

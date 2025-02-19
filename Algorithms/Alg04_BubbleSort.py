cfd732ee83899fbe323f9820433053a73fb7261b
joshmaglione <joshmaglione@gmail.com>
2025-02-19 13:49:22 +0000

    bare-bones bubble

--- Algorithms/Alg04_BubbleSort.py
+++ Algorithms/Alg04_BubbleSort.py
@@ -1,9 +1,20 @@
+# Slightly fancy
 def BubbleSort(L:list) -> list:
 	swapped = True
 	while swapped:
 		swapped = False
 		for i in range(len(L) - 1):
-			if L[i] > L[i+1]:
+			if L[i] > L[i + 1]:
 				swapped = True
-				(L[i], L[i+1]) = (L[i+1], L[i])
+				(L[i], L[i + 1]) = (L[i + 1], L[i])
 	return L
+
+
+# Bare-bones
+def BubbleSort_bare(L:list) -> list:
+    n = len(L)
+    for i in range(n):
+        for j in range(n - i - 1):
+            if L[j] > L[j + 1]:
+                (L[j], L[j + 1]) = (L[j + 1], L[j])
+    return L

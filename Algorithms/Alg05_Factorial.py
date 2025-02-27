a6bba03993cfcbe0f79e4439a286e5830383fcbe
joshmaglione <joshmaglione@gmail.com>
2025-02-27 11:04:34 +0000

    edits

--- Algorithms/Alg05_Factorial.py
+++ Algorithms/Alg05_Factorial.py
@@ -1,15 +1,16 @@
-# We assume that n is a positive integer throughout
+# We assume that n is a non-negative integer throughout
 
 # Definition 1: non-recursive
-def factorial1(n:int) -> int:
+def factorial1(n):
 	f = 1
 	for i in range(1, n + 1):
-		f *= i
+		f *= i					# f = f * i
 	return f
 
+# x += 1		MEANS 		x = x + 1
 
 # Definition 2: recursive
 def factorial2(n:int) -> int:
-	if n == 1:						# base case
+	if n == 0:						# base case
 		return 1
 	return n * factorial2(n - 1)	# recursion

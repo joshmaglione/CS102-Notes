f79a7f8f53e6fd567bc0ddec2973722794579655
joshmaglione <joshmaglione@gmail.com>
2025-03-12 13:19:39 +0000

    comparison

--- Algorithms/Alg10_MergeSort.py
+++ Algorithms/Alg10_MergeSort.py
@@ -1,23 +1,25 @@
 # Subroutine of merge sort. 
 # Given two sorted lists, we merge them and sort.
-def merge(left:list, right:list) -> list:
+def Merge(left:list, right:list) -> list:
     lr_merge = [] 
-    while len(left) > 0 and len(right) > 0:
-        if left[0] < right[0]:
-            lr_merge.append(left[0])
-            left = left[1:]
+    i = 0
+    j = 0
+    while len(left) > i and len(right) > j:
+        if left[i] < right[j]:
+            lr_merge.append(left[i])
+            i += 1
         else:
-            lr_merge.append(right[0])
-            right = right[1:]
-    lr_merge.extend(left)
-    lr_merge.extend(right)
+            lr_merge.append(right[j])
+            j += 1
+    lr_merge.extend(left[i:])
+    lr_merge.extend(right[j:])
     return lr_merge
 
 # Given an unsorted list, return a sorted list
-def merge_sort(L:list) -> list:
+def MergeSort(L:list) -> list:
     if len(L) <= 1:
         return L
     mid = len(L) // 2
-    left = merge_sort(L[:mid])
-    right = merge_sort(L[mid:])
-    return merge(left, right)
+    left = MergeSort(L[:mid])
+    right = MergeSort(L[mid:])
+    return Merge(left, right)
